0c326b1926c0257e9b3b1763472bb9fc584df7f5
hcook99 <hjamcook@gmail.com>
2020-01-08 01:58:13 -0500

    Initial Commit

---
+++ NotesCopy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesCopy
+//
+//  Created by Harrison Cook on 1/8/20.
+//  Copyright © 2020 Harrison Cook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
