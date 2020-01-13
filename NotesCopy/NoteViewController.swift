6740051a048a1d30e03f9b0ab098fdef0017f37c
hcook99 <hjamcook@gmail.com>
2020-01-13 15:43:05 -0500

    Finished first ios app

---
+++ NotesCopy/NoteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NoteViewController.swift
+//  NotesCopy
+//
+//  Created by Harrison Cook on 1/9/20.
+//  Copyright © 2020 Harrison Cook. All rights reserved.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
